975cfd7317147d70bdbe51e13ab18beee2020ebf
Faiq <mfaiqn@gmail.com>
2021-11-29 04:18:29 +0700

    collections

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -5,32 +5,30 @@
     </header>
     <ul>
       <Task
-        v-for="task in getTasks()"
-        v-bind:key="task._id"
-        v-bind:task="task"
+          v-for="task in tasks"
+          v-bind:key="task._id"
+          v-bind:task="task"
       />
     </ul>
   </div>
 </template>
 
 <script>
-  import Vue from "vue";
-  import Task from "./components/Task.vue";
+import Vue from "vue";
+import Task from "./components/Task.vue";
+import { TasksCollection } from "../api/TasksCollection";
 
-  export default {
-      components: {
-        Task
-      },
-      data() {
-          return {};
-      },
-      methods: {
-      getTasks() {
-      return [
-        { _id: 1, text: "This is task 1" },
-        { _id: 2, text: "This is task 2" },
-        { _id: 3, text: "This is task 3" }
-      ];
+export default {
+  components: {
+    Task
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  meteor: {
+    tasks() {
+      return TasksCollection.find({}).fetch();
     }
   }
 };
