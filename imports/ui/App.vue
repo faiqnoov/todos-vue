16f05de0160cf5a758dcd6c13ecd8a15c42d8414
Faiq <mfaiqn@gmail.com>
2021-12-02 01:20:45 +0700

    filter tasks

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -3,12 +3,21 @@
     <header>
       <div className="app-bar">
         <div className="app-header">
-          <h1>📝️ To Do List</h1>
+          <h1>
+            📝️ To Do List
+            <span v-if="incompleteCount > 0">({{ incompleteCount }})</span>
+          </h1>
         </div>
       </div>
     </header>
     <div class="main">
       <TaskForm />
+      <div class="filter">
+        <button v-model="hideCompleted" @click="toggleHideCompleted">
+          <span v-if="hideCompleted">Show All</span>
+          <span v-else>Hide Completed Tasks</span>
+        </button>
+      </div>
       <ul class="tasks">
         <Task
           class="task"
@@ -26,18 +35,37 @@ import Vue from "vue";
 import Task from "./components/Task.vue";
 import TaskForm from "./components/TaskForm.vue";
 import { TasksCollection } from "../api/TasksCollection";
+
 export default {
   components: {
     Task,
     TaskForm,
   },
   data() {
-    return {};
+    return {
+      hideCompleted: false,
+    };
   },
-  methods: {},
+  methods: {
+    toggleHideCompleted() {
+      this.hideCompleted = !this.hideCompleted;
+    },
+  },
   meteor: {
     tasks() {
-      return TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch();
+      let filteredTasks = TasksCollection.find(
+        {},
+        { sort: { createdAt: -1 } }
+      ).fetch();
+
+      if (this.hideCompleted) {
+        filteredTasks = filteredTasks.filter((task) => !task.checked);
+      }
+
+      return filteredTasks;
+    },
+    incompleteCount() {
+      return TasksCollection.find({ checked: { $ne: true } }).count();
     },
   },
 };
